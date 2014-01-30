541c0d808d58092c32e961d9cd3754f1f3053b64
Quentin Mathé <quentin.mathe@gmail.com>
2014-01-30 11:59:55 +0000

    Fixed Graphviz support for versions older than 2.30.
    DocGraphWriter now supports the two Graphviz libraries:
    - libgraph (version < 2.30)
    - libcgraph (version >= 2.30) 
    
    Note: Versions older than 2.23 are unsupported.
    
    
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Developer/Services/DocGenerator@10184 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- DocGraphWriter.m
+++ DocGraphWriter.m
@@ -2,18 +2,34 @@
 #include <unistd.h>
 #include "DocGraphWriter.h"
 
+/*
+ * For writing code that supports both libgraph and libcgraph, see 
+ * http://www.graphviz.org/dot.demo/example.c 
+ */
 @implementation DocGraphWriter
 
 - (id) init
 {
 	self = [super init];
+
 	mNodes = [NSMutableDictionary new];
 	mEdges = [NSMutableArray new];
+
+	// NOTE: No need to call aginit() just for libgraph, since we use a context 
+	// to support the graph layout, see gvLayout() in -layout.
 	mGraphContext = gvContext();
+#ifdef WITH_CGRAPH
 	mGraph = agopen("g", Agdirected, NULL);
+#else
+	mGraph = agopen("g", AGDIGRAPH);
+#endif
+	ETAssert(mGraphContext != NULL);
+	ETAssert(mGraph != NULL);
+
 	[self setGraphAttribute: @"rankdir" with: @"BT"];
 	[self setGraphAttribute: @"size" with: @"2."];
 	[self setGraphAttribute: @"dpi" with: @"72"];
+
 	return self;
 }
 
@@ -58,11 +74,20 @@
 
 - (NSValue*) addNode: (NSString*) node
 {
+	NILARG_EXCEPTION_TEST(node);
+	ETAssert(mGraph != NULL);
+
 	NSValue* pointer = [mNodes objectForKey: node];
 	if (pointer)
 		return pointer;
 
+	//NSLog(@"Add node %@", node);
+
+#ifdef WITH_CGRAPH
 	Agnode_t *n = agnode(mGraph, (char*)[node UTF8String], 1);
+#else
+	Agnode_t *n = agnode(mGraph, (char*)[node UTF8String]);
+#endif
 	NSValue* value = [NSValue valueWithPointer: n];
 	[mNodes setObject: value forKey: node];
 	return value;
@@ -70,15 +95,29 @@
 
 - (void) addEdge: (NSString*) nodeA to: (NSString*) nodeB
 {
+	NILARG_EXCEPTION_TEST(nodeA);
+	NILARG_EXCEPTION_TEST(nodeB);
+	ETAssert(mGraph != NULL);
+
+	//NSLog(@"Add edge from %@ to %@", nodeA, nodeB);
+
 	NSValue* A = [self addNode: nodeA];
 	NSValue* B = [self addNode: nodeB];
+#ifdef WITH_CGRAPH
 	agedge(mGraph, [A pointerValue], [B pointerValue], "", 1);
+#else
+	agedge(mGraph, [A pointerValue], [B pointerValue]);
+#endif
 }
 
 - (void) setAttribute: (NSString*) attribute
 		 with: (NSString*) value
 		   on: (NSString*) node
 {
+	NILARG_EXCEPTION_TEST(attribute);
+	NILARG_EXCEPTION_TEST(value);
+	NILARG_EXCEPTION_TEST(node);
+
 	NSValue* n = [self addNode: node];
 	agsafeset([n pointerValue],
 		 (char*) [attribute UTF8String],
@@ -88,6 +127,10 @@
 - (void) setGraphAttribute: (NSString*) attribute
 	              with: (NSString*) value
 {
+	NILARG_EXCEPTION_TEST(attribute);
+	NILARG_EXCEPTION_TEST(value);
+	ETAssert(mGraph != NULL);
+
 	agsafeset(mGraph,
 		 (char*) [attribute UTF8String],
 		 (char*) [value UTF8String], "");
--- GNUmakefile
+++ GNUmakefile
@@ -4,10 +4,30 @@ CC = clang
 
 TOOL_NAME = etdocgen
 
-$(TOOL_NAME)_CPPFLAGS += -DWITH_CGRAPH=1  
-$(TOOL_NAME)_OBJCFLAGS += -fobjc-arc -Wparentheses `pkg-config libcgraph --cflags` `pkg-config libgvc --cflags` 
-$(TOOL_NAME)_TOOL_LIBS = -lEtoileFoundation -lSourceCodeKit `pkg-config libcgraph --libs` `pkg-config libgvc --libs`
+#
+# Graphviz Support
+#
+# For version >= 2.30, libcgraph is stable and we use it. For these recent 
+# versions, libgvc must link libcgraph, otherwise something is wrong in the host 
+# sytem Graphviz packages.
+#
+# For version < 2.30, libcgraph is unstable or not available, and we use 
+# libgraph.
+# For these older versions, libgvc could link either just libgraph, or both 
+# libgraph and libcgraph (but we just ignore libcgraph in this case, because it 
+# tends to crash on some systems e.g. Ubuntu 12.04).
+#
+# For version < 2.23, libgraph is too old and is not supported. 
+#
+ifeq (`pkg-config --atleast-version=2.30`, 0)
+  $(TOOL_NAME)_CPPFLAGS += -DWITH_CGRAPH=1 
+endif
 
+$(TOOL_NAME)_OBJCFLAGS += -fobjc-arc -Wparentheses `pkg-config libgvc --cflags`
+# For Graphviz version >= 2.30, we expect pkg-config to return -lcgraph (never -lgraph)
+# For example, Fedora could require a workaround, see https://bugzilla.redhat.com/show_bug.cgi?id=904790
+$(TOOL_NAME)_TOOL_LIBS = -lEtoileFoundation -lSourceCodeKit `pkg-config libgvc --libs` 
+ 
 # For SourceCodeKit dependencies
 $(TOOL_NAME)_CPPFLAGS += -I`llvm-config --src-root`/tools/clang/include/ -I`llvm-config --includedir`
 $(TOOL_NAME)_TOOL_LIBS += -lgnustep-gui
